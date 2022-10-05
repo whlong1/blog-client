4f2969cbfbafd5514914edd4593b7a3f8387c114
whlong1 <whunterlong@gmail.com>
2022-10-04 23:50:02 -0400

    import icon asssets

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,4 +1,12 @@
+import News from '../../assets/icons/News.svg'
+import Music from '../../assets/icons/Music.svg'
+import Games from '../../assets/icons/Games.svg'
+import Movies from '../../assets/icons/Movies.svg'
+import Sports from '../../assets/icons/Sports.svg'
+import Televison from '../../assets/icons/Television.svg'
+
 const Icon = ({ category }) => {
+  
   return <img src="" alt="" />
 }
 
