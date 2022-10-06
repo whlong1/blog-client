5f2ec377cec11f3a37f2693f61cedb524161ba2d
whlong1 <whunterlong@gmail.com>
2022-10-06 11:45:34 -0400

    adjust landing css

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,9 @@ import Logotype from '../../assets/branding/logotype.svg'
 const Landing = () => {
   return (
     <main className={styles.container}>
-      <img src={Logotype} alt="A cute owl" />
+      <section>
+        <img src={Logotype} alt="A cute owl" />
+      </section>
       {/* As a night owl, I have a hard time finding blogging apps that fit my lifestyle. */}
     </main>
   )
