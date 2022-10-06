a6e78fa331310a38c0a1013316bcd8d6818578ff
whlong1 <whunterlong@gmail.com>
2022-10-06 11:33:21 -0400

    add svg background

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,6 +5,7 @@ const Landing = () => {
   return (
     <main className={styles.container}>
       <img src={Logotype} alt="A cute owl" />
+      {/* As a night owl, I have a hard time finding blogging apps that fit my lifestyle. */}
     </main>
   )
 }
