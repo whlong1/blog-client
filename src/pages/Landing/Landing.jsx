5bf0ecf73576459d1b27eed5f92641821a636a96
whlong1 <whunterlong@gmail.com>
2022-10-07 00:57:47 -0400

    style testimonials section

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,26 +6,32 @@ import Profile from '../../assets/branding/profile.png'
 const Landing = () => {
   return (
     <main className={styles.container}>
-      <section>
+
+      <section className={styles.splash}>
         <img src={Logotype} alt="A cute owl" />
       </section>
+
       <section className={styles.about}>
-        <h4>HOO ARE YOU?</h4>
+        <h3>HOO ARE YOU?</h3>
         <h1>ABOOT HOOT</h1>
       </section>
+
       <section className={styles.testimonial}>
-        {/* <h4>WHO GIVES A HOOT?</h4>
-        <h1>TESTIMONIALS</h1> */}
+
+        <header>
+          <h3>WHO GIVES A HOOT?</h3>
+          <h1>TESTIMONIALS</h1>
+        </header>
 
         <article>
           <header>
             <img id={styles.prof} src={Profile} alt="A satisfied user" />
-            <h3>Ben Manley</h3>
+            <h4>Ben Manley</h4>
             <p>Software Engineer</p>
           </header>
           <p>
-            As a night owl, I have a hard time finding blogging apps that fit my lifestyle. <br/>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br/>
+            As a night owl, I have a hard time finding blogging apps that fit my lifestyle. <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor <br />
             incididunt ut labore et dolore magna aliqua.
           </p>
           <footer>
