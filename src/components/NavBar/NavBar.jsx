4414fdb78c56f9a864b1d525bd807195d9e2f8be
whlong1 <whunterlong@gmail.com>
2022-10-14 13:27:15 -0400

    fix navbar subcomponents

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,31 +4,26 @@ import Logo from '../../assets/branding/logo.svg'
 
 const NavBar = ({ user, handleLogout }) => {
 
-  const publicLinks = () => {
-    return (
-      <ul>
-        <li><Link to="/login">LOG IN</Link></li>
-        <li><Link to="/signup">SIGN UP</Link></li>
-      </ul>
-    )
-  }
+  const publicLinks = (
+    <ul>
+      <li><Link to="/login">LOG IN</Link></li>
+      <li><Link to="/signup">SIGN UP</Link></li>
+    </ul>
+  )
 
-  const protectedLinks = () => {
-    return (
-      <ul>
-
-        <li><Link to="/blogs">BLOGS</Link></li>
-        <li><Link to="/blogs/new">NEW BLOG</Link></li>
-        <li><Link to="/profiles">PROFILES</Link></li>
-        <li><Link to="/logout" onClick={handleLogout}>LOG OUT</Link></li>
-      </ul>
-    )
-  }
+  const protectedLinks = (
+    <ul>
+      <li><Link to="/blogs">BLOGS</Link></li>
+      <li><Link to="/blogs/new">NEW BLOG</Link></li>
+      <li><Link to="/profiles">PROFILES</Link></li>
+      <li><Link to="/logout" onClick={handleLogout}>LOG OUT</Link></li>
+    </ul>
+  )
 
   return (
     <nav className={styles.container}>
       <Link to={'/'}><img src={Logo} alt="A cute owl" /></Link>
-      {user ? protectedLinks() : publicLinks()}
+      {user ? protectedLinks : publicLinks}
     </nav>
   )
 }
