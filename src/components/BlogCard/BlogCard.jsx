245a18597cb44951fb77ae360871916cc0c30c11
whlong1 <whunterlong@gmail.com>
2022-10-14 11:08:21 -0400

    fix authortab css

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom"
+import styles from './BlogCard.module.css'
+
+// Components
 import Icon from "../Icon/Icon"
+import AuthorTab from "../AuthorTab/AuthorTab"
 
 const BlogCard = ({ blog }) => {
   return (
-    <article>
+    <article className={styles.container}>
       <header>
         <h1>{blog.title}</h1>
-        <h3>{blog.author.name}</h3>
-        <Icon category={blog.category} />
+        <AuthorTab content={blog} />
+        {/* <Icon category={blog.category} /> */}
       </header>
-      <p>{blog.text}</p>
+      {/* <p>{blog.text}</p> */}
       <footer>
         <Link to={`/blogs/${blog._id}`} state={blog}>
           View Details
