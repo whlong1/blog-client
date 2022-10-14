8b28b3c7f1b5524b84b0f383996af821cc346fa0
whlong1 <whunterlong@gmail.com>
2022-10-14 07:31:59 -0400

    add initial styling to profiles component

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,9 +1,9 @@
 import styles from './UserCard.module.css'
 import ProfileIcon from '../../assets/icons/profile.png'
 
-const UserCard = ({user}) => {
+const UserCard = ({ user }) => {
   const avatar = user.avatar ? user.avatar : ProfileIcon
-  return(
+  return (
     <div className={styles.container}>
       <img src={avatar} alt="The user's avatar" />
       <h3>{user.name}</h3>
--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -1,5 +1,9 @@
 import { useState, useEffect } from 'react'
+import styles from './Profiles.module.css'
 import * as profileService from '../../services/profileService'
+
+// Components
+import UserCard from '../../components/UserCard/UserCard'
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
@@ -12,20 +16,15 @@ const Profiles = () => {
     fetchProfiles()
   }, [])
 
+  if (!profiles.length) return <p>No Profiles</p>
+
   return (
-    <>
-      <h1>Hello. This is a list of all the profiles.</h1>
-      {profiles.length ? 
-        <>
-          {profiles.map(profile =>
-            <p key={profile._id}>{profile.name}</p>
-          )}
-        </>
-      :
-        <p>No profiles yet</p>
-      }
-    </>
+    <main className={styles.container}>
+      {profiles.map(profile => (
+        <UserCard key={profile._id} user={profile} />
+      ))}
+    </main>
   )
 }
- 
+
 export default Profiles
