3fb6548dbb28c047a84d1f4ab847190664d25b60
whlong1 <whunterlong@gmail.com>
2022-10-15 13:01:39 -0400

    add conditional rendering for author ui

--- src/pages/BlogDetails/BlogDetails.jsx
+++ src/pages/BlogDetails/BlogDetails.jsx
@@ -39,8 +39,12 @@ const BlogDetails = (props) => {
           <h1>{blog.title}</h1>
           <span>
             <AuthorTab content={blog} />
-            <Link to={`/blogs/${id}/edit`} state={blog}>Edit</Link>
-            <button onClick={() => props.handleDeleteBlog(id)}>Delete</button>
+            {blog.author._id === props.user.profile &&
+              <>
+                <Link to={`/blogs/${id}/edit`} state={blog}>Edit</Link>
+                <button onClick={() => props.handleDeleteBlog(id)}>Delete</button>
+              </>
+            }
           </span>
         </header>
         <p>{blog.text}</p>
