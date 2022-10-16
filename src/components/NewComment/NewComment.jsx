a295d34d24e2979f0323c79a1f00e01b31c80731
whlong1 <whunterlong@gmail.com>
2022-10-16 04:06:20 -0400

    add create svg icon

--- src/components/NewComment/NewComment.jsx
+++ src/components/NewComment/NewComment.jsx
@@ -3,7 +3,7 @@ import styles from './NewComment.module.css'
 
 const NewComment = ({ handleAddComment }) => {
   const [form, setForm] = useState({ text: '' })
-  const [visible, setVisible] = useState(false)
+  // const [visible, setVisible] = useState(false)
 
   const handleChange = ({ target }) => {
     setForm({ ...form, [target.name]: target.value })
@@ -13,31 +13,30 @@ const NewComment = ({ handleAddComment }) => {
     e.preventDefault()
     handleAddComment(form)
     setForm({ text: '' })
-    setVisible(false)
+    // setVisible(false)
   }
 
   return (
     <form className={styles.container} onSubmit={handleSubmit}>
-      <span>
-        <h3>New Comment</h3>
+      {/* <span>
         <button type="button" onClick={() => setVisible(!visible)}>
           {visible ? "-" : "+"}
         </button>
-      </span>
-      {visible &&
-        <>
-          <textarea
-            required
-            type="text"
-            name="text"
-            id="text-input"
-            value={form.text}
-            placeholder="Text"
-            onChange={handleChange}
-          />
-          <button type="submit">SUBMIT</button>
-        </>
-      }
+      </span> */}
+      {/* {visible && */}
+      {/* <> */}
+      <textarea
+        required
+        type="text"
+        name="text"
+        id="text-input"
+        value={form.text}
+        placeholder="Add a Comment"
+        onChange={handleChange}
+      />
+      <button type="submit">SUBMIT</button>
+      {/* </> */}
+      {/* } */}
     </form>
   )
 }
