73f961d57d9730f94855460315114b170bfd3205
whlong1 <whunterlong@gmail.com>
2022-10-17 15:50:10 -0400

    add try catch blocks to service functions

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -3,85 +3,117 @@ import * as tokenService from '../services/tokenService'
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/blogs`
 
 const index = async () => {
-  const res = await fetch(BASE_URL, {
-    headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
-  })
-  return res.json()
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const show = async (id) => {
-  const res = await fetch(`${BASE_URL}/${id}`, {
-    headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
-  })
-  return res.json()
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const create = async (blogData) => {
-  const res = await fetch(BASE_URL, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(blogData)
-  })
-  return res.json()
+  try {
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blogData)
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const update = async (blogData) => {
-  const res = await fetch(`${BASE_URL}/${blogData._id}`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(blogData)
-  })
-  return res.json()
+  try {
+    const res = await fetch(`${BASE_URL}/${blogData._id}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(blogData)
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const deleteBlog = async (id) => {
-  const res = await fetch(`${BASE_URL}/${id}`, {
-    method: 'DELETE',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`
-    }
-  })
-  return res.json()
+  try {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      }
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const createComment = async (id, commentData) => {
-  const res = await fetch(`${BASE_URL}/${id}/comments`, {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(commentData)
-  })
-  return res.json()
+  try {
+    const res = await fetch(`${BASE_URL}/${id}/comments`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(commentData)
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const updateComment = async (blogId, commentId, commentData) => {
-  const res = await fetch(`${BASE_URL}/${blogId}/comments/${commentId}`, {
-    method: 'PUT',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(commentData)
-  })
-  return res.json()
+  try {
+    const res = await fetch(`${BASE_URL}/${blogId}/comments/${commentId}`, {
+      method: 'PUT',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(commentData)
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 const deleteComment = async (blogId, commentId) => {
-  const res = await fetch(`${BASE_URL}/${blogId}/comments/${commentId}`, {
-    method: 'DELETE',
-    headers: {
-      'Authorization': `Bearer ${tokenService.getToken()}`
-    }
-  })
-  return res.json()
+  try {
+    const res = await fetch(`${BASE_URL}/${blogId}/comments/${commentId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      }
+    })
+    return res.json()
+  } catch (err) {
+    throw err
+  }
 }
 
 export {
