f7001add6f401d19b36b7352c76fd9c414b04f37
whlong1 <whunterlong@gmail.com>
2022-10-21 18:22:16 -0400

    add new ui icons

--- src/components/Interactions/Interactions.jsx
+++ src/components/Interactions/Interactions.jsx
@@ -1,30 +1,26 @@
 import { Link } from "react-router-dom"
-import Liked from '../../assets/icons/liked.svg'
 
 // Components
 import Icon from "../Icon/Icon"
 import AuthorInfo from "../../components/AuthorInfo/AuthorInfo"
 
 const Interactions = ({ id, user, blog, handleDeleteBlog }) => {
-  const LikedIcon = <img src={Liked} alt="A heart" />
 
   const authorOptions = (
     <>
       <Link to={`/blogs/${id}/edit`} state={blog}>
-        {/* <Icon category="Edit" /> */}
-        Edit
+        <Icon category="Edit" />
       </Link>
       <button onClick={() => handleDeleteBlog(id)}>
-        {/* <Icon category="Trash" /> */}
-        Delete
+        <Icon category="Trash" />
       </button>
     </>
   )
 
   const readerOptions = (
     <>
-      {/* <button><Icon category={"Comments"} /></button>
-      <button><Icon category={"Like"} /></button> */}
+      <button><Icon category={"Comments"} /></button>
+      <button><Icon category={"Like"} /></button>
     </>
   )
 
